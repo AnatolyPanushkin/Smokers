61967d23e42270ca4e6f8066ae2730fa96240bda
Anatoly <anatolypanushkin@mail.ru>
2021-12-09 13:53:06 +0300

    complete version!

--- Smokers/Program.cs
+++ Smokers/Program.cs
@@ -10,66 +10,66 @@ namespace Smokers
         private static Mutex _tabaccoMutex = new Mutex();
         private static Mutex _matchesMutex = new Mutex();
         private static Mutex _paperMutex = new Mutex();
+        private static Mutex _allWait = new Mutex();
         static void Main(string[] args)
         {
             Thread thread;
-            thread = new Thread(Servant);
+            thread= new Thread(Servant);
             thread.Start();
             thread.Join();
+            
+            
+            
+            
 
         }
 
         static async void Servant()
         {
-            int v1 = new Random().Next(0, 3);
-            int v2 = new Random().Next(0, 3);
-            if (v1 == 0 & v2 == 1)
-            {
-                await Paper();
-            }
+            int v1 = new Random().Next(1, 4);
+            int v2 = new Random().Next(1, 4);
+            //1 - бумага
+            //2 - спички
+            //3 - табак
 
-            if (v1 == 1 & v2 == 2)
-            {
-                await Tabacco();
-            }
-
-            if (v1 == 0 & v2 == 2)
-            {
-                await Matches();
-            }
-            else
-            {
-                Servant();
-            }
-
+            await Tabacco(v1, v2);
+            await Paper(v1, v2);
+            await Matches(v1, v2);
+            
             Servant();
         }
 
-        static Task Tabacco()
+        static async Task Tabacco(int v1,int v2)
         {
-            //_tabaccoMutex.WaitOne();
-            ConsoleHelper.WriteToConsole("Курильщик с табаком курит");
-            Thread.Sleep(2000);
-           // _tabaccoMutex.ReleaseMutex();
-            return Task.CompletedTask;
+            if ((v1==1 & v2==2) || (v1==2 & v2==1 ))
+            {
+                _allWait.WaitOne();
+                ConsoleHelper.WriteToConsole("Курильщик с табаком курит");
+                Thread.Sleep(2000);
+                _allWait.ReleaseMutex(); 
+            }
         }
         
-        static Task Matches()
+        static async Task Matches(int v1,int v2)
         {
-           // _matchesMutex.WaitOne();
-            ConsoleHelper.WriteToConsole("Курильщик со спичками курит");
-            Thread.Sleep(2000);
-            //_matchesMutex.ReleaseMutex();
-            return Task.CompletedTask;
+            if ((v1 == 1 & v2 == 3) || (v1 == 3 & v2 == 1))
+            {
+                _allWait.WaitOne();
+                ConsoleHelper.WriteToConsole("Курильщик со спичками курит");
+                Thread.Sleep(2000);
+                _allWait.ReleaseMutex();
+            }
         }
 
-        static Task Paper()
+        static async Task Paper(int v1,int v2)
         {
-           // _paperMutex.WaitOne();
-            ConsoleHelper.WriteToConsole("Курильщик с бумагой курит");
-            Thread.Sleep(2000);
-            //_paperMutex.ReleaseMutex();
-            return Task.CompletedTask;
+            if ((v1 == 2 & v2 == 3) || (v1 == 3 & v2 == 2))
+            {
+                _allWait.WaitOne();
+                ConsoleHelper.WriteToConsole("Курильщик с бумагой курит");
+                Thread.Sleep(2000);
+                _allWait.ReleaseMutex();
+            }
         }
         
     }
